8f9f7cc60f842ecb299ed3e89b0b0307487ab93d
WildAndrewLee <andrew@wildandrewlee.com>
2016-01-14 14:19:06 -0500

    spaces

--- bot.py
+++ bot.py
@@ -9,92 +9,92 @@ from db import add_user, clear_users, get_users, remove_user
 from secrets import secrets
 
 if '--debug' in sys.argv:
-	__DEBUG__ = True
+    __DEBUG__ = True
 elif '-d' in sys.argv:
-	__DEBUG__ = True
+    __DEBUG__ = True
 else:
-	__DEBUG__ = False
+    __DEBUG__ = False
 
 delay = 15000 # 15000ms or 15s
 ping_cmd = '{ping} -c 1 {address}'
 
 def check(address):
-	ping = subprocess.check_output('which ping', shell=True).strip()
-	cmd = ping_cmd.format(**locals())
+    ping = subprocess.check_output('which ping', shell=True).strip()
+    cmd = ping_cmd.format(**locals())
 
-	if __DEBUG__: print cmd
+    if __DEBUG__: print cmd
 
-	try:
-		subprocess.check_output(cmd, shell=True)
-		return True
-	except:
-		return False		
+    try:
+        subprocess.check_output(cmd, shell=True)
+        return True
+    except:
+        return False        
 
 class Bot(object):
-	server = None
-	channel = None
-	client = None
+    server = None
+    channel = None
+    client = None
 
-	def __init__(self):
-		self.client = Client()
+    def __init__(self):
+        self.client = Client()
 
-	def alert(self):
-		users = get_users()
+    def alert(self):
+        users = get_users()
 
-		for user in users:
-			user = User('', user, '', '')
-			self.client.send_message(user, 'MapleStory is back online!')
+        for user in users:
+            user = User('', user, '', '')
+            self.client.send_message(user, 'MapleStory is back online!')
 
-		clear_users()
+        clear_users()
 
-	def run(self):
-		self.client.login(secrets.email, secrets.password)
+    def run(self):
+        self.client.login(secrets.email, secrets.password)
 
-		@self.client.event
-		def on_message(message):
-			if message.channel.is_private:
-				if message.content == 'cancel':
-					remove_user(message.author)
-					self.client.send_message(
-						message.author,
-						'You have been removed from the alert list.'
-					)
-			elif any([m == self.client.user for m in message.mentions]):
-					if 'subscribe' in message.content:
-						add_user(message.author)
-						self.client.send_message(
-							message.author,
-							dedent(
-								'''
-								You are now on the alert list.
-								Send "cancel" in private message to remove yourself from the list.
-								'''
-							).strip()
-						)
+        @self.client.event
+        def on_message(message):
+            if message.channel.is_private:
+                if message.content == 'cancel':
+                    remove_user(message.author)
+                    self.client.send_message(
+                        message.author,
+                        'You have been removed from the alert list.'
+                    )
+            elif any([m == self.client.user for m in message.mentions]):
+                    if 'subscribe' in message.content:
+                        add_user(message.author)
+                        self.client.send_message(
+                            message.author,
+                            dedent(
+                                '''
+                                You are now on the alert list.
+                                Send "cancel" in private message to remove yourself from the list.
+                                '''
+                            ).strip()
+                        )
 
-					if 'help' in message.content:
-						self.client.send_message(
-							message.channel,
-							dedent(
-								'''
-								This bot will PM you when MapleStory is back online.
-								All inquiries should be sent to Reticence via PM.
-								'''
-							).strip()
-						)
+                    if 'help' in message.content:
+                        self.client.send_message(
+                            message.channel,
+                            dedent(
+                                '''
+                                This bot will PM you when MapleStory is back online.
+                                All inquiries should be sent to Reticence via PM.
+                                '''
+                            ).strip()
+                        )
 
-		thread = Thread(target=self.client.run)
-		thread.daemon = True
-		thread.start()
+        thread = Thread(target=self.client.run)
+        thread.daemon = True
+        thread.start()
 
-		while True:
-			if all([check(addr) for addr in login_address]):
-				if __DEBUG__: print 'maple online'
-				self.alert()
-			else:
-				if __DEBUG__: print 'not online'
-			
-			time.sleep(delay)
+        while True:
+            if all([check(addr) for addr in login_address]):
+                if __DEBUG__: print 'maple online'
+                self.alert()
+            else:
+                if __DEBUG__: print 'not online'
+            
+            time.sleep(delay)
 
 bot = Bot()
 bot.run()
