c48f7e5fb7758569d0f24a297848528fdc5052db
WildAndrewLee <andrew@wildandrewlee.com>
2016-01-14 14:51:59 -0500

    spaces

--- db.py
+++ db.py
@@ -3,21 +3,21 @@ import redis
 __db = redis.Redis('localhost')
 
 def add_user(user):
-	__db.append('to_notify', ' ' + str(user.id))
+    __db.append('to_notify', ' ' + str(user.id))
 
 def get_users():
-	users = str(__db.get('to_notify'))
-	return users.strip().split(' ') if users is not None else []
+    users = str(__db.get('to_notify'))
+    return users.strip().split(' ') if users is not None else []
 
 def clear_users():
-	__db.delete('to_notify')
+    __db.delete('to_notify')
 
 def remove_user(user):
-	users = get_users()
-	
-	try:
-		users.remove(str(user.id))
-	except:
-		pass
+    users = get_users()
+    
+    try:
+        users.remove(str(user.id))
+    except:
+        pass
 
-	__db.set('to_notify', ' '.join(users))
+    __db.set('to_notify', ' '.join(users))
